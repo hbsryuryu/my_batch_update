f1b74f857652b52e68bb2763ab7956c9ecbd9abb
hbsryuryu <hbsryuryu@gmail.com>
2026-04-21 14:13:03 +0900

    fix http

--- httpCallWebApp/__init__.py
+++ httpCallWebApp/__init__.py
@@ -1,27 +1,7 @@
 import azure.functions as func
-import logging
-import os
 import json
-import datetime
-import uuid
-
-from azure.search.documents import SearchClient
-from azure.core.credentials import AzureKeyCredential
-from azure.search.documents.models import VectorizedQuery
-
-from openai import AzureOpenAI
-from dotenv import load_dotenv
-
-from sqlalchemy import create_engine, Column,Integer, String, Boolean, DateTime, Text, ForeignKey, JSON, inspect
-from sqlalchemy.orm import scoped_session, sessionmaker, relationship
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.exc import SQLAlchemyError, IntegrityError
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
-
-# .env ファイルの読み込み
-load_dotenv(override=True)
-
 
 
 @app.route(route="test", methods=["GET"])
